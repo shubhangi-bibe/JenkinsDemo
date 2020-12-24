0afffe55b4034bd83b1d0f791d9a4f2cf1300b13
Shubhangi Bibe <shubhangibibe@gmail.com>
2020-12-24 16:16:07 +0530

    first commit

---
+++ spring-boot-app/src/main/java/com/capgemini/springboot/controller/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.capgemini.springboot.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
